cdbb269024afc94aca630c71d30c2d9287adbe00
yliu <yliu@XLIJFELKNIX.local>
2024-07-01 18:14:03 +0900

    [feat] ex03

--- ex03/main.go
+++ ex03/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"ft"
+	"os"
+)
+
+func ftLen(args []string) int {
+	i := 0
+	for range args {
+		i++
+	}
+	return i
+}
+
+func ftPutStr(str string) {
+	for _, c := range str {
+		ft.PrintRune(c)
+	}
+}
+
+func ftPutStrLn(str string) {
+	ftPutStr(str)
+	ft.PrintRune('\n')
+}
+
+func displayFile(filename string) {
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		ftPutStrLn("ERROR: " + err.Error())
+	}
+	ftPutStr(string(contents))
+}
+
+func main() {
+	if ftLen(os.Args) == 1 {
+		buf := make([]byte, 42)
+		for {
+			n, err := os.Stdin.Read(buf)
+			if err != nil || n == 0 {
+				return
+			}
+			ftPutStr(string(buf[:n]))
+		}
+	}
+	for _, filename := range os.Args[1:] {
+		displayFile(filename)
+	}
+}
